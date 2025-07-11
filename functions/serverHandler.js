d7ed70f393c8a3408f47c21800ae0e9055c98caf
rm792 <rizwanmehmood262@gmail.com>
2025-07-11 21:47:31 +0500

    .

--- functions/serverHandler.js
+++ functions/serverHandler.js
@@ -15,7 +15,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors());
-app.use(`${basePath}/api/inngest`, serve({ client: inngest, functions }));
+app.use(`${basePath}/inngest`, serve({ client: inngest, functions }));
 app.use(clerkMiddleware());
 
 
