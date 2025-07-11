506d186698c3fddc49303733a89fbe46cd2dff79
rm792 <rizwanmehmood262@gmail.com>
2025-07-11 21:39:09 +0500

    .

--- functions/serverHandler.js
+++ functions/serverHandler.js
@@ -13,6 +13,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(`${basePath}/api/inngest`, serve({ client: inngest, functions }));
 app.use(clerkMiddleware());
 
 // Routes for Netlify Function path
@@ -26,6 +27,5 @@ app.get(`${basePath}/status`, (_req, res) => {
     res.send("Server is live");
 });
 
-app.use(`${basePath}/api/inngest`, serve({ client: inngest, functions }));
 
 export const handler = ServerlessHttp(app);
