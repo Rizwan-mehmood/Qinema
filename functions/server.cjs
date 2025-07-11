ae71541ca7ca92a99489060a92232df1afbcce7d
rm792 <rizwanmehmood262@gmail.com>
2025-07-11 22:10:30 +0500

    .

--- functions/server.cjs
+++
@@ -1,4 +0,0 @@
-exports.handler = async function (event, context) {
-    const { handler } = await import('./serverHandler.js');
-    return handler(event, context);
-};
--- server/configs/db.js
+++ server/configs/db.js
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
 
 const connectDB = async () => {
-    if (isConnected) return;
-
     try {
         mongoose.connection.on('connected', () => console.log("Database Connected"));
         mongoose.connect(`${process.env.MONGODB_URI}`);
-        isConnected = true;
     } catch (error) {
         console.error(error);
     }
